58bd8c4815e094a1cf471f1db2104565a01a675f
NikolasSkrlj <nikolas.skrlj@hotmail.com>
2021-07-07 20:15:50 +0200

    integrated formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,88 +1,14 @@
-import * as React from "react";
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-  Container,
-  Flex,
-  Heading,
-  FormControl,
-  FormLabel,
-  Input,
-  FormHelperText,
-  Button,
-  Divider,
-  InputLeftElement,
-  InputGroup,
-  InputRightElement,
-} from "@chakra-ui/react";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
-export const App = () => {
-  const [isShownPassword, setIsShownPassword] = React.useState(false);
+import React, { useState, useEffect } from "react";
 
+import { ChakraProvider, theme, Flex } from "@chakra-ui/react";
+
+import { LoginForm } from "./components/LoginForm";
+export const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <ChakraProvider theme={theme}>
       <Flex justify="center" align="center" w="100%" h="100vh" p="5">
-        <Box
-          maxW="lg"
-          w="md"
-          border="1px"
-          borderColor="gray.200"
-          px="10"
-          py="15"
-          borderRadius="xl"
-          boxShadow="md"
-        >
-          <Heading fontSize="x-large" textAlign="center" my="5">
-            Prijavi se
-          </Heading>
-          <VStack spacing="4">
-            <FormControl id="email">
-              <FormLabel>Email</FormLabel>
-              <Input mx="1.5" type="email" spellCheck="false" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Lozinka</FormLabel>
-              <InputGroup>
-                <InputRightElement
-                  cursor="pointer"
-                  onClick={() => setIsShownPassword(!isShownPassword)}
-                  children={
-                    isShownPassword ? (
-                      <FaEyeSlash color="lightgrey" size="20px" />
-                    ) : (
-                      <FaEye color="lightgrey" size="20px" />
-                    )
-                  }
-                  pr="3"
-                />
-                <Input
-                  mx="1.5"
-                  type={isShownPassword ? "text" : "password"}
-                  pr="40"
-                  spellCheck="false"
-                />
-              </InputGroup>
-
-              <FormHelperText mx="1.5">
-                <Link color="teal.500" fontSize="xs">
-                  Zaboravili ste lozinku?
-                </Link>
-              </FormHelperText>
-            </FormControl>
-          </VStack>
-          <Button my="10" w="100%" colorScheme="teal">
-            Prijava
-          </Button>
-          <Text fontSize="sm" textAlign="center">
-            Nemaš račun? <Link color="teal.400">Registriraj se</Link>
-          </Text>
-        </Box>
+        <LoginForm setIsLoggedIn={setIsLoggedIn} />
       </Flex>
     </ChakraProvider>
   );
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,147 @@
+import React, { useState, useEffect } from "react";
+
+import {
+  Box,
+  Text,
+  Link,
+  VStack,
+  Flex,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  Button,
+  InputGroup,
+  InputRightElement,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Formik, Form, Field } from "formik";
+import * as yup from "yup";
+
+type TLoginFormProps = {
+  setIsLoggedIn: (flag: boolean) => any;
+};
+const validationSchema = new yup.ObjectSchema({
+  email: yup
+    .string()
+    .email("Unesite ispravan email!")
+    .required("Email je obvezno polje!"),
+  password: yup.string().required("Lozinka je obavezno polje!"),
+});
+
+export const LoginForm = (props: TLoginFormProps) => {
+  const { setIsLoggedIn } = props;
+  const [isShownPassword, setIsShownPassword] = useState(false);
+
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          actions.setSubmitting(false);
+          setIsLoggedIn(true);
+        }, 1500);
+      }}
+    >
+      {(props) => (
+        <Form>
+          <Box
+            maxW="lg"
+            w="md"
+            border="1px"
+            borderColor="gray.200"
+            px="10"
+            py="15"
+            borderRadius="xl"
+            boxShadow="md"
+          >
+            <Heading fontSize="x-large" textAlign="center" my="5">
+              Prijavi se
+            </Heading>
+
+            <VStack spacing="4">
+              <Field name="email">
+                {({ field, form }: any) => (
+                  <FormControl
+                    id="email"
+                    isInvalid={form.errors.email && form.touched.email}
+                  >
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                      id="email"
+                      mx="1.5"
+                      type="email"
+                      disabled={props.isSubmitting}
+                      spellCheck="false"
+                      placeholder="example@domain.com"
+                      {...field}
+                    />
+                    <FormErrorMessage mx="1.5">
+                      {form.errors.email}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="password">
+                {({ field, form }: any) => (
+                  <FormControl
+                    id="password"
+                    isInvalid={form.errors.password && form.touched.password}
+                  >
+                    <FormLabel>Lozinka</FormLabel>
+                    <InputGroup>
+                      <InputRightElement
+                        cursor="pointer"
+                        onClick={() => setIsShownPassword(!isShownPassword)}
+                        children={
+                          isShownPassword ? (
+                            <FaEyeSlash color="lightgrey" size="20px" />
+                          ) : (
+                            <FaEye color="lightgrey" size="20px" />
+                          )
+                        }
+                        pr="3"
+                      />
+                      <Input
+                        mx="1.5"
+                        type={isShownPassword ? "text" : "password"}
+                        disabled={props.isSubmitting}
+                        pr="40"
+                        spellCheck="false"
+                        {...field}
+                      />
+                    </InputGroup>
+                    <FormErrorMessage mx="1.5">
+                      {form.errors.password}
+                    </FormErrorMessage>
+                    <FormHelperText mx="1.5">
+                      <Link color="teal.500" fontSize="xs">
+                        Zaboravili ste lozinku?
+                      </Link>
+                    </FormHelperText>
+                  </FormControl>
+                )}
+              </Field>
+            </VStack>
+            <Button
+              mb={10}
+              mt={7}
+              colorScheme="teal"
+              isLoading={props.isSubmitting}
+              type="submit"
+              isFullWidth
+            >
+              Prijava
+            </Button>
+            <Text fontSize="sm" textAlign="center">
+              Nemaš račun? <Link color="teal.400">Registriraj se</Link>
+            </Text>
+          </Box>
+        </Form>
+      )}
+    </Formik>
+  );
+};
