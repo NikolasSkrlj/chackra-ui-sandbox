f49ac8fb9514c1d0133c2e1fcb4b8e3e975c2c00
NikolasSkrlj <nikolas.skrlj@hotmail.com>
2021-07-07 20:35:51 +0200

    added welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import React, { useState, useEffect } from "react";
 import { ChakraProvider, theme, Flex } from "@chakra-ui/react";
 
 import { LoginForm } from "./components/LoginForm";
+import { WelcomePage } from "./components/WelcomePage";
 export const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <ChakraProvider theme={theme}>
       <Flex justify="center" align="center" w="100%" h="100vh" p="5">
-        <LoginForm setIsLoggedIn={setIsLoggedIn} />
+        {isLoggedIn ? (
+          <WelcomePage setIsLoggedIn={setIsLoggedIn} />
+        ) : (
+          <LoginForm setIsLoggedIn={setIsLoggedIn} />
+        )}
       </Flex>
     </ChakraProvider>
   );
---
+++ src/components/WelcomePage.tsx
@@ -0,0 +1,53 @@
+import { Box, Heading, Text, Button, Flex, Image } from "@chakra-ui/react";
+import React from "react";
+import { useState } from "react";
+
+type TWelcomePageProps = {
+  setIsLoggedIn: (flag: boolean) => any;
+};
+
+export const WelcomePage = (props: TWelcomePageProps) => {
+  const { setIsLoggedIn } = props;
+  const [isLogoutClicked, setIsLogoutClicked] = useState(false);
+  return (
+    <Box
+      maxW="lg"
+      w="md"
+      border="1px"
+      borderColor="gray.200"
+      px="10"
+      py="15"
+      borderRadius="xl"
+      boxShadow="md"
+    >
+      <Heading fontSize="x-large" textAlign="center" my="5">
+        Dobrodošao, Nikolas!
+      </Heading>
+
+      <Text textAlign="center">
+        Uspješno si se prijavio, evo slika mačke :D
+      </Text>
+      <Image
+        borderRadius="xl"
+        my="5"
+        src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg"
+      ></Image>
+      <Flex justify="center" mt="6" mb="2">
+        <Button
+          variant="outline"
+          colorScheme="teal"
+          isLoading={isLogoutClicked}
+          onClick={() => {
+            setIsLogoutClicked(true);
+            setTimeout(() => {
+              setIsLogoutClicked(false);
+              setIsLoggedIn(false);
+            }, 1500);
+          }}
+        >
+          Odjavi se
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
